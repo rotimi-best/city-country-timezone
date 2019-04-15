37b537341f8487981e777cb3cad8881df342ebda
Rotimi Best <rotimiibitoyebest@yahoo.co.uk>
2019-04-15 22:34:31 +0300

    Improved city checks + time dif calculation

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const cityTimezones = require('city-timezones');
 
+const groupCountries = [
+  { caseSensitive: false, name: 'Russia', result: 'Russia'  },
+  { caseSensitive: false, name: 'Miami', result:  'Miami' },
+  { caseSensitive: false, name: 'Vancouver', result:  'Vancouver' },
+  { caseSensitive: false, name: 'Dallas', result: 'Dallas'  },
+  { caseSensitive: false, name: 'Copenhagen', result: 'Denmark' },
+  { caseSensitive: false, name: 'San Francisco', result: 'California' },
+  { caseSensitive: false, name: 'California', result: 'California' },
+  { caseSensitive: false, name: 'Amsterdam', result: 'Netherlands' },
+  { caseSensitive: false, name: 'Warwickshire', result: 'United Kingdom' },
+  { caseSensitive: false, name: 'Zurich', result: 'Switzerland'  },
+  { caseSensitive: false, name: 'Armonk', result: 'New York'  },
+  { caseSensitive: false, name: 'Quebec', result: 'New York'  },
+  { caseSensitive: false, name: 'Research Triangle Park', result: 'New York' },
+  { caseSensitive: false, name: 'NEUILLY', result: 'France'  },
+  { caseSensitive: false, name: 'Paris', result: 'France'  },
+  { caseSensitive: false, name: 'Norway', result: 'Oslo'  },
+  { caseSensitive: false, name: 'Los Angeles', result: 'California' },
+  { caseSensitive: false, name: 'Slovak', result: 'Slovakia' },
+  { caseSensitive: false, name: 'Toronto', result: 'Ontario' },
+  { caseSensitive: false, name: 'London', result: 'United Kingdom' },
+  { caseSensitive: true, name: 'USA', result: 'California' },
+  { caseSensitive: true, name: 'NY', result: 'New York' },
+  { caseSensitive: true, name: 'NC', result: 'New York' }
+];
+
 /**
  * 
  * Returns length of a string object
@@ -22,6 +48,7 @@ const setTimeZone = cityData => {
     }
 
     const [{ timezone }] = cityData;
+
     return timezone;
 }
 
@@ -33,46 +60,19 @@ const setTimeZone = cityData => {
  * @returns {string}
  */
 const determineCountry = country => {
-    if (!country) {
-        return "";
+  if (!country) {
+    return "";
+  }
+
+  for (const groupCountry of groupCountries) {
+    const { caseSensitive, name, result } = groupCountry;
+    
+    if (RegExp(name, caseSensitive ? '' : 'i').test(country)) {
+      return result;
     }
+  }
 
-    const russia = RegExp('Russia', 'i').test(country);
-    const miami = RegExp('Miami', 'i').test(country);
-    const dallas = RegExp('Dallas', 'i').test(country);
-    const denmark = RegExp('Copenhagen', 'i').test(country);
-    const california = RegExp('USA', 'i').test(country);
-    const london = RegExp('England', 'i').test(country);
-    const england = RegExp('London', 'i').test(country);
-    const warwickshire = RegExp('Warwickshire', 'i').test(country);
-    const zurich = RegExp('Zurich', 'i').test(country);
-    const armok = RegExp('Armonk', 'i').test(country);
-    const quebec = RegExp('Quebec', 'i').test(country);
-    const rtp = RegExp('Research Triangle Park', 'i').test(country);
-    const france = RegExp('NEUILLY', 'i').test(country);
-    const paris = RegExp('Paris', 'i').test(country);
-    const ny = RegExp('NY', 'i').test(country);
-    const nc = RegExp('NC', 'i').test(country);
-
-    if (russia) country = "Russia";
-
-    if (france || paris) country = "France";
-
-    if (miami) country = "Miami";
-
-    if (dallas) country = "Dallas";
-
-    if (denmark) country = "Denmark";
-
-    if (california) country = "California";
-
-    if (london || england || warwickshire) country = "United Kingdom";
-
-    if (zurich) country = "Switzerland";
-
-    if (armok || rtp || quebec || ny || nc) country = "Armonk, New York";
-
-    return country;
+  return country;
 };
 
 /**
@@ -84,34 +84,36 @@ const determineCountry = country => {
  */
 const getTimezones = givenCountry => {
     if (!givenCountry) {
-        return "";
+      return "";
     }
 
-    const country = determineCountry(givenCountry);
-    const cityData = cityTimezones.findFromCityStateProvince(country);
+    const cityData = cityTimezones.findFromCityStateProvince(givenCountry);
 
     let timezone = "";
-    
-    if (len(cityData)) timezone = setTimeZone(cityData);
 
-    else {
-        const countryParts = country.split(',');
-        const region = countryParts[len(countryParts) - 1];
-        const regionData = cityTimezones.findFromCityStateProvince(region);
+    if (len(cityData)) {
+      timezone = setTimeZone(cityData);
+    } else {
+      const country = determineCountry(givenCountry);
+      const countryParts = country.split(',');
+      const region = countryParts[len(countryParts) - 1];
+      const regionData = cityTimezones.findFromCityStateProvince(region);
 
-        if (len(regionData)) timezone = setTimeZone(regionData);
+      if (len(regionData)) {
+        timezone = setTimeZone(regionData);
+      } else {
+        const invalidRegions = ["Greater", "Bay", "Area", "area", "Metro", "City"];
+        const splittedRegion = region.split(' ');
+        const province = splittedRegion
+          .filter(reg  => !invalidRegions.includes(reg) && len(reg))
+          .join(' ');
 
-        else {
-            const invalidRegions = ["Greater", "Bay", "Area", "area", "Metro", "City"];
-            const splittedRegion = region.split(' ');
-            const province = splittedRegion
-                                .filter(reg  => !invalidRegions.includes(reg) && len(reg))
-                                .join(' ');
+        const provinceData = cityTimezones.findFromCityStateProvince(province);
 
-            const provinceData = cityTimezones.findFromCityStateProvince(province);
-
-            if (len(provinceData)) timezone = setTimeZone(provinceData);
+        if (len(provinceData)) {
+          timezone = setTimeZone(provinceData);
         }
+      }
     }
 
     return timezone;
@@ -126,13 +128,14 @@ const getTimezones = givenCountry => {
  */
 const getTimeNowFromTz = timeZone => {
     if (!timeZone) {
-        return "";
+      return "";
     }
 
     const now = new Date();
-    const dateOfLead = now.toLocaleString('en-GB', {hour: 'numeric',   hour12: false, timeZone })
+    const dateOfLead = now.toLocaleString('en-GB', { timeZone });
+    const millisec = new Date(dateOfLead).getTime();
 
-    return dateOfLead;
+    return millisec;
 };
 
 /**
@@ -144,12 +147,13 @@ const getTimeNowFromTz = timeZone => {
  */
 const calcTimeDiff = timeFromTz => {
     if (!timeFromTz) {
-        return 0;
+      return 0;
     }
-    
-    const dateFromTimeZone = new Date();
-    const timeDiff =  timeFromTz - (dateFromTimeZone.getHours());
-    
+
+    const odessaTime = Date.now();
+    const diff = timeFromTz - odessaTime;
+    const timeDiff = Math.round(diff / 1000 / 60 / 60);
+
     return timeDiff;
 };
 
@@ -161,22 +165,22 @@ const calcTimeDiff = timeFromTz => {
  * @returns {Object}
  */
 const getTimeDiffAndTimeZone = country => {
-    let timeDiffAndTimeZone = { timezone: "", time_diff: "0" };
-    const timezone = getTimezones(country);
+  let timeDiffAndTimeZone = { timezone: "", time_diff: "0" };
+  const timezone = getTimezones(country);
 
-    if (timezone) {
-        const timeFromTimeZone = getTimeNowFromTz(timezone);
-        let time_diff = calcTimeDiff(timeFromTimeZone).toString();
-        
-        // Not "-1" and Not "0" but if "1"
-        if (time_diff.charAt(0) !== "0" && time_diff.charAt(0) !== "-") {
-            time_diff = "+" + time_diff;
-        }
-
-        timeDiffAndTimeZone = { timezone, time_diff };
+  if (timezone) {
+    const timeFromTimeZone = getTimeNowFromTz(timezone);
+    let time_diff = calcTimeDiff(timeFromTimeZone).toString();
+    
+    // Not "-1" and Not "0" but if "1"
+    if (time_diff.charAt(0) !== "0" && time_diff.charAt(0) !== "-") {
+      time_diff = "+" + time_diff;
     }
 
-    return timeDiffAndTimeZone;
+    timeDiffAndTimeZone = { timezone, time_diff };
+  }
+
+  return timeDiffAndTimeZone;
 };
 
 module.exports = getTimeDiffAndTimeZone;
